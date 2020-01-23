ba3d5ff15be282bb17b2adc11d665274b84f4df6
Miguel <gimenezgimenezmiguel@gmail.com>
2020-01-23 06:41:02 +0100

    remove action

--- src/Services/__mocks__/ApiClient.js
+++ src/Services/__mocks__/ApiClient.js
@@ -1,17 +1,24 @@
 export default {
 	getDiscoverMovies: (category) => {
-		return Promise.resolve([
-			{
-				id: 475557,
+		return new Promise((resolve,reject) => {
+			setTimeout(() => {
+				resolve([
+					{
+						id: 475557,
+	
+						title: 'Joker'
+					},
+					{
+						id: 290859,
+	
+						title: 'Terminator: Dark Fate'
+					}
+				]);
+			}, 3000);
 
-				title: 'Joker'
-			},
-			{
-				id: 290859,
+	
+		})
 
-				title: 'Terminator: Dark Fate'
-			}
-		]);
 	},
 	getCategories: () => {
 		return ['movies'];
